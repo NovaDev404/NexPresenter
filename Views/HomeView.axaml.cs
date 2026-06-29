9c21c0b649bcc2b6dd95704f0cd221bd2d3336b4
NovaDev404 <novadev404@novadev.vip>
2026-06-29 15:51:24 +0930

    fix command bindings

--- Views/HomeView.axaml.cs
+++ Views/HomeView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using NexPresenter.Models;
+using NexPresenter.ViewModels;
 
 namespace NexPresenter.Views;
 
@@ -7,5 +10,27 @@ public partial class HomeView : UserControl
     public HomeView()
     {
         InitializeComponent();
+    }
+
+    private void OnOpenClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.Tag is Presentation presentation)
+        {
+            if (DataContext is HomeViewModel viewModel)
+            {
+                viewModel.SelectPresentationCommand.Execute(presentation);
+            }
+        }
+    }
+
+    private void OnDeleteClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button button && button.Tag is Guid presentationId)
+        {
+            if (DataContext is HomeViewModel viewModel)
+            {
+                viewModel.DeletePresentationCommand.Execute(presentationId);
+            }
+        }
     }
 }
